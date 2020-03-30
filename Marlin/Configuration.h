18ce937764cae440081281f7e57e0847f2320323
Sebastian Hammerl <github@omoco.de>
2020-03-30 21:31:58 +0200

    renamed default PID values

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -483,10 +483,15 @@
 
   // If you are using a pre-configured hotend then you can use one of the value sets by uncommenting it
 
-  // Sidewinder Genius
+  // Artillery Genius
   #define DEFAULT_Kp 12.42
   #define DEFAULT_Ki 0.85
   #define DEFAULT_Kd 45.26
+
+  // Ultimaker
+  //#define DEFAULT_Kp 22.2
+  //#define DEFAULT_Ki 1.08
+  //#define DEFAULT_Kd 114
 
   // MakerGear
   //#define DEFAULT_Kp 7.0
@@ -533,11 +538,16 @@
   //#define MIN_BED_POWER 0
   //#define PID_BED_DEBUG // Sends debug data to the serial port.
 
-  //120V 250W silicone heater into 4mm borosilicate (MendelMax 1.5+)
-  //from FOPDT model - kp=.39 Tp=405 Tdead=66, Tc set to 79.2, aggressive factor of .15 (vs .1, 1, 10)
+  // Artillery Genius
   #define DEFAULT_bedKp 31.71
   #define DEFAULT_bedKi 3.99
   #define DEFAULT_bedKd 168.01
+
+  //120V 250W silicone heater into 4mm borosilicate (MendelMax 1.5+)
+  //from FOPDT model - kp=.39 Tp=405 Tdead=66, Tc set to 79.2, aggressive factor of .15 (vs .1, 1, 10)
+  //#define DEFAULT_bedKp 10.00
+  //#define DEFAULT_bedKi .023
+  //#define DEFAULT_bedKd 305.4
 
   //120V 250W silicone heater into 4mm borosilicate (MendelMax 1.5+)
   //from pidautotune

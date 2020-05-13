193d369afcc99cf1a0ee36314611b885b3369072
Sebastian Hammerl <github@omoco.de>
2020-05-13 09:11:11 +0200

    Tuned E values

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -740,7 +740,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80.121, 80.121, 399.778, 445 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80.5, 80.5, 400, 435 }
 
 /**
  * Default Max Feed Rate (mm/s)
